8db0d9be6a80a7c3fc19293e70bbdc2868aa6c42
NaKia Whitby <whitby.nakia@gmail.com>
2018-11-02 18:31:09 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -3,14 +3,3 @@ def using_times
 	  puts "Wingardium Leviosa"
 	end
 end
-
-jewels_in_bag = 100
-
-3.times do
-  puts "Hiding 10 stolen jewels."
-  jewels_in_bag = jewels_in_bag - 10
-  puts "Now there are only #{jewels_in_bag} jewels left to hide!"
-end
-# => 3 (return value)
-
-puts "We have #{jewels_in_bag} jewels still to hide!"
